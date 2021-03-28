ae16bf0bdd16d24c2c94a2ff42435c4f571a083a
shidetake <shidetake@gmail.com>
2021-03-28 23:25:53 +0900

    rebuilding site 2021年 3月28日 日曜日 23時25分53秒 JST

--- index.js
+++ index.js
@@ -1,226 +1,226 @@
 var data = [
 {
-url: "http://shidetake.com/google_auth/",
+url: "https://shidetake.com/google_auth/",
 title: "任意のGoogle APIをRubyで使うときの手順",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/active_support_time/",
+url: "https://shidetake.com/active_support_time/",
 title: "素のRubyで日時を扱う",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/blu_ripping/",
+url: "https://shidetake.com/blu_ripping/",
 title: "MakeMKVとHandBrakeによるBlu-lay Discリッピング",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/heroku_reset/",
+url: "https://shidetake.com/heroku_reset/",
 title: "Heroku上のRailsアプリのDBをリセットする",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/dropcast/",
+url: "https://shidetake.com/dropcast/",
 title: "Dropboxで自分専用のPodcast配信",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/reins_map/",
+url: "https://shidetake.com/reins_map/",
 title: "地域ごとのマンション単価をGoogleマップで見る",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/geoapi/",
+url: "https://shidetake.com/geoapi/",
 title: "Yahoo!ジオコーダAPIで住所から経度緯度を取得",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/aws_cli/",
+url: "https://shidetake.com/aws_cli/",
 title: "AWSのRDSインスタンスをCLIで操作",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/big_image/",
+url: "https://shidetake.com/big_image/",
 title: "画像を任意のサイズ（容量）に変換する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/heroku_domains/",
+url: "https://shidetake.com/heroku_domains/",
 title: "Herokuの公開URLをコマンドで確認する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/alexa_node_red1/",
+url: "https://shidetake.com/alexa_node_red1/",
 title: "AlexaとNode-REDを利用したスマートホームスキル作成時の注意",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/pyatv/",
+url: "https://shidetake.com/pyatv/",
 title: "Apple TVをRaspberry Piで操作",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/gmail2line/",
+url: "https://shidetake.com/gmail2line/",
 title: "Gmailに重要なメールが届いたらLINEに通知する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/line_feeding/",
+url: "https://shidetake.com/line_feeding/",
 title: "定期的なリマインドをLINE BOTにやってもらう",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/make_cron/",
+url: "https://shidetake.com/make_cron/",
 title: "Makefileでcronを登録する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/alexa_skill_1/",
+url: "https://shidetake.com/alexa_skill_1/",
 title: "Alexa SkillsでHello worldする",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/git_showtool/",
+url: "https://shidetake.com/git_showtool/",
 title: "git showの差分をvimdiffで見る",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/ebroad/",
+url: "https://shidetake.com/ebroad/",
 title: "マンション付属の光回線が早くなった話",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/make_win/",
+url: "https://shidetake.com/make_win/",
 title: "WindowsでもLinuxでも動くMakefileを書く",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/git_difftool/",
+url: "https://shidetake.com/git_difftool/",
 title: "gitのdiffをvimdiffで見る",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/speedtest/",
+url: "https://shidetake.com/speedtest/",
 title: "スピードテストを定期的に実行して回線速度の推移を見る",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/mysql_install/",
+url: "https://shidetake.com/mysql_install/",
 title: "Ubuntu 16.04にmysqlをインストールする",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/dasher_systemd/",
+url: "https://shidetake.com/dasher_systemd/",
 title: "Raspberry PiでSystemdを使ってdasherをサービス化",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/ddns/",
+url: "https://shidetake.com/ddns/",
 title: "DDNSでLANの外から自宅のRaspberry PiにSSH接続する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/stock_trendline_1/",
+url: "https://shidetake.com/stock_trendline_1/",
 title: "株価チャートからトレンドラインを引く 1",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/google_finance_api/",
+url: "https://shidetake.com/google_finance_api/",
 title: "Google Finance APIを使って株価チャートを作成",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/iij_coupon/",
+url: "https://shidetake.com/iij_coupon/",
 title: "IIJmioの追加クーポンをLINE botに買ってもらう",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/blog_img/",
+url: "https://shidetake.com/blog_img/",
 title: "IFTTTとGoogle Photoを連携するとブログ用の画像準備がはかどる",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/icam/",
+url: "https://shidetake.com/icam/",
 title: "iPadをウェブカメラ化してiCamSourceと連携",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/wireless_tv/",
+url: "https://shidetake.com/wireless_tv/",
 title: "テレビのアンテナ線を無線化する",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/google_photo_api_2/",
+url: "https://shidetake.com/google_photo_api_2/",
 title: "Picasa APIを使ってGoogle Photoからデジカメ写真だけを取得 2",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/google_photo_api_1/",
+url: "https://shidetake.com/google_photo_api_1/",
 title: "Picasa APIを使ってGoogle Photoからデジカメ写真だけを取得 1",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/ripping_3/",
+url: "https://shidetake.com/ripping_3/",
 title: "HandBrakeCLIによるDVDリッピング 3",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/ripping_2/",
+url: "https://shidetake.com/ripping_2/",
 title: "HandBrakeCLIによるDVDリッピング 2",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/ripping_1/",
+url: "https://shidetake.com/ripping_1/",
 title: "HandBrakeCLIによるDVDリッピング 1",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/gmail_api_2/",
+url: "https://shidetake.com/gmail_api_2/",
 title: "RubyでGmail本文を取得する 2",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/gmail_api_1/",
+url: "https://shidetake.com/gmail_api_1/",
 title: "RubyでGmail本文を取得する 1",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/line_highlights_4/",
+url: "https://shidetake.com/line_highlights_4/",
 title: "KindleのハイライトをLINEに通知する 4",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/line_highlights_3/",
+url: "https://shidetake.com/line_highlights_3/",
 title: "KindleのハイライトをLINEに通知する 3",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/line_highlights_2/",
+url: "https://shidetake.com/line_highlights_2/",
 title: "KindleのハイライトをLINEに通知する 2",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/line_highlights_1/",
+url: "https://shidetake.com/line_highlights_1/",
 title: "KindleのハイライトをLINEに通知する 1",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/time_capsule/",
+url: "https://shidetake.com/time_capsule/",
 title: "Time Machineの容量制限",
 content: "[]"
 }
 ,{
-url: "http://shidetake.com/dash_button/",
+url: "https://shidetake.com/dash_button/",
 title: "Amazon Dash ButtonからLINE通知",
 content: "[dasher導入 他の人が詳しく書いているので割愛。 個人的にはAmazon Dash Button と slackを連携させるという記事がとても参考になった。 LINEへのPUSH通知設定 LINE側の設定はLINE BOTでPUSH通知するを参照。 CHANNEL_ACCESS_TOKENとUSERIDは上記事で取得したもの。 MAC_ADDRESSはdasher導入時に取得したものを書くこと。 {&amp;quot;buttons&amp;quot;: [ { &amp;quot;name&amp;quot;: &amp;quot;joy&amp;quot;, &amp;quot;address&amp;quot;: &amp;quot;MAC_ADDRESS&amp;quot;, &amp;quot;url&amp;quot;: &amp;quot;https://api.line.me/v2/bot/message/push&amp;quot;, &amp;quot;method&amp;quot;: &amp;quot;POST&amp;quot;, &amp;quot;headers&amp;quot;: { &amp;quot;Content-Type&amp;quot;: &amp;quot;application/json&amp;quot;, &amp;quot;Authorization&amp;quot;: &amp;quot;Bearer {CHANNEL_ACCESS_TOKEN}&amp;quot;}, &amp;quot;json&amp;quot;: true, &amp;quot;body&amp;quot;: { &amp;quot;to&amp;quot;: &amp;quot;USERID&amp;quot;, &amp;quot;messages&amp;quot;: [ { &amp;quot;type&amp;quot;: &amp;quot;text&amp;quot;, &amp;quot;text&amp;quot;: &amp;quot;JOY!&amp;quot; } ] } } ]}]"
 }
 ,{
-url: "http://shidetake.com/line_push/",
+url: "https://shidetake.com/line_push/",
 title: "LINE BOTでPUSH通知する",
 content: "[登録 PUSH通知する場合はDeveloper Trialで登録 Google Apps ScriptでユーザID取得 友達に追加したユーザのIDを登録したメールアドレスにメールする 以下のコードでウェブアプリケーションとして導入する CHANNEL_ACCESS_TOKENはLINE depelopersで取得できる example@gmail.comは任意のgmailアドレス アプリケーションにアクセスできるユーザーは全員（匿名ユーザーを含む） var CHANNEL_ACCESS_TOKEN = &#39;CHANNEL_ACCESS_TOKEN&#39;; function doPost(e) { Logger.log(&#39;doPost&#39;) var events = JSON.parse(e.postData.contents).events; events.forEach (function(event) { if (event.type == &amp;quot;follow&amp;quot;) { mailUserId(event); } }); } function mailUserId(e) { MailApp.sendEmail(&#39;example@gmail.com&#39;, &#39;mailId&#39;, e.source.userId); } curlでMessaging APIのPUSH通知を使う CHANNEL_ACCESS_TOKENは上で使ったものと同じ USERIDはプッシュ先 今回は上で取得した自分のユーザIDを使う curl -X POST \ -H &#39;Content-Type:application/json&#39; \ -H &#39;Authorization: Bearer {CHANNEL_ACCESS_TOKEN}&#39; \ -d &#39;{ &amp;quot;to&amp;quot;: &amp;quot;USERID&amp;quot;, &amp;quot;messages&amp;quot;:[ { &amp;quot;type&amp;quot;: &amp;quot;text&amp;quot;, &amp;quot;text&amp;quot;: &amp;quot;Hello, world!&amp;quot; } ] }&#39; https://api.line.me/v2/bot/message/push 関連ポスト Amazon Dash ButtonからLINE通知]"
 }
 ,{
-url: "http://shidetake.com/post/",
+url: "https://shidetake.com/post/",
 title: "Posts",
 content: "[]"
 }]
